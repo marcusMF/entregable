69286452984da51da631631cac08583280f8b626
marcusMF <marcusmf2110@gmail.com>
2024-10-09 20:20:08 -0500

    entregable1

---
+++ Java/validation.js
@@ -0,0 +1,18 @@
+// Validación de los formularios con Bootstrap
+(function () {
+  "use strict";
+  var forms = document.querySelectorAll(".needs-validation");
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
